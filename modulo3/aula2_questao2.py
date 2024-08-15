c13ebdd8c48b9d88031b3eef49f22c6019eb6c94
Kadubd <166766854+Kadubd@users.noreply.github.com>
2024-08-15 20:33:17 -0300

    Gabarito aula 3.2

---
+++ modulo3/aula2_questao2.py
@@ -0,0 +1,4 @@
+ju = int(input("Qual a idade de Juliana?" )) #lê a idade de juliana
+cris = int(input("Qual a idade de Cris?" )) #lê a idade de cris
+resultado = ju>=18 or cris>=18 #descobre se é verdadeiro ou falso, se um for verdadeiro será "true" se ambos forem falsos será "false"
+print(resultado)#imprime o resultado 

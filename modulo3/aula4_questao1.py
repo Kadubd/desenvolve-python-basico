94326e5f99fb59c3995d01c7f2e2c832bf16e952
Kadubd <166766854+Kadubd@users.noreply.github.com>
2024-08-15 20:50:12 -0300

    Delete modulo3/aula4_questao1.py

--- modulo3/aula4_questao1.py
+++
@@ -1,6 +0,0 @@
-n1 = int(input("digite um numero ")) #Lê o primeiro número digitado
-n2 = int(input("digite um numero ")) #Lê o segundo nome digitado
-if n1+n2 % 2==0: #soma os dois numeros e calcula se o numero é divisivel por 2
-    print("O resultado é par") #se sim imprime 
-else:
-    print("O resultado é ímpar") #se não imprime 

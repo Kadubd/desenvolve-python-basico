602f4d0c1ec22a0f112d62f83e26c493d22dc29b
Kadubd <166766854+Kadubd@users.noreply.github.com>
2024-08-15 20:50:32 -0300

    Delete modulo3/aula4_questao3.py

--- modulo3/aula4_questao3.py
+++
@@ -1,5 +0,0 @@
-ano = int(input("Digite um ano... ")) #Lê o ano
-if ano%4==0 and ano%100!=0 or ano%400==0: #calcula se o ano e divisivel por 4 e não é dividido por 100 ou se ele é divisivel por 400
-    print("Bissexto!") #imprime caso o ano seja bissexto
-else: #Caso o ano não cumpra nenhum dos requisitos ele não é bissextpo
-    print("Não Bissexto!!") #imprime que ele não é bissexto 

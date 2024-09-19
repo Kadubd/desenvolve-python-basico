b17d696c3c55845d618049d8b9bfaddd99db9b1c
Kadubd <166766854+Kadubd@users.noreply.github.com>
2024-09-19 19:02:00 -0300

    Delete Modulo 6/aula2_questao2.py

--- Modulo 6/aula2_questao2.py
+++
@@ -1,6 +0,0 @@
-import random
-num_elementos = (random.randint(5, 20) for i in range(1))
-elementos = [random.randint(1, 10) for i in range(num_elementos)]
-print(f"lista: {elementos}")
-print(f"Soma dos valores da lista: {sum(elementos)}")
-print(f"Média dos valores da lista: {sum(elementos)/len(elementos)}")
